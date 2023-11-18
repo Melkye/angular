c18a3c3f4acd681758bc744cda19912ec3b061ba
Denys Adamov <melkye777@gmail.com>
2023-11-18 11:57:15 +0200

    added all code to lab 1

---
+++ lab1/binding2/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  //standalone: true,
+  template: `
+    <!-- exercise 6 -->
+    <div [class.isredbox]="isRed"></div>
+    <div [class.isredbox]="!isRed"></div>
+    <input type="checkbox" [(ngModel)]="isRed" />
+    <div [class]="blue"></div>
+
+    <!-- exercise 7 -->
+    <div [style.backgroundColor]="isyellow ? 'yellow' : 'blue'"></div>
+    <div [style.background-color]="!isyellow ? 'yellow' : 'blue'"></div>
+    <input type="checkbox" [(ngModel)]="isyellow" />
+  `,
+
+  styles: [
+    `
+      /*exercise 6*/
+      div {
+        width: 50px;
+        height: 50px;
+        border: 1px solid #ccc;
+      }
+      .isredbox {
+        background-color: red;
+      }
+      .isbluebox {
+        background-color: blue;
+      }
+    `,
+  ],
+})
+export class AppComponent {
+  // exercise 6
+  title = 'binding2';
+  isRed = false;
+  blue = 'isbluebox';
+
+  // exercise 7
+  isyellow = false;
+}
