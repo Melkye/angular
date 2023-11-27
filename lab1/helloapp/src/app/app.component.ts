e8bf2638d26842973a29dd03c39ce84ada357a8e
Denys Adamov <melkye777@gmail.com>
2023-11-27 12:27:35 +0200

    remove unused files and code

--- lab1/helloapp/src/app/app.component.ts
+++ lab1/helloapp/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'my-app',
-  // standalone: true,
-  // imports: [CommonModule, RouterOutlet],
-  // templateUrl: './app.component.html',
   template: `<label>Введіть назву:</label>
     <input [(ngModel)]="name" placeholder="name" />
     <h1>Ласкаво просимо {{ name }}!</h1>`,
-  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = '';
