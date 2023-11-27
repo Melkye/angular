d658f86879a08b456e0aa52666716546a9e708a5
Denys Adamov <melkye777@gmail.com>
2023-11-27 23:15:19 +0200

    finish exercice 4 of lab 2

--- lab2/components1/src/app/app.component.ts
+++ lab2/components1/src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: ` <label>Введіть ім'я:</label>
+    <input [(ngModel)]="name" placeholder="name" />
+    <h1>Ласкаво просимо {{ name }}!</h1>
+    <h2>Hello Angular</h2>
+    <p>Angular 16 представляє модульну архітектуру додатку</p>
+    <child-comp></child-comp>
+    <p>Hello {{ name }}</p>`,
+  styles: [
+    `
+      h2,
+      p {
+        color: #333;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  name: '';
+  name = 'Петро';
 }
