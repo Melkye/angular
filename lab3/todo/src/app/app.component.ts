1f561c9bc94e761ee1f040c5b1ac8640b3f2a0d3
Denys Adamov <melkye777@gmail.com>
2023-12-03 00:03:02 +0200

    init lab3

---
+++ lab3/todo/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  private list = new TodoList('Тарас', [
+    new TodoItem('Зробити пробіжку', true),
+    new TodoItem('Купити квіти'),
+    new TodoItem('Забрати квитки'),
+  ]);
+  get username(): string {
+    return this.list.user;
+  }
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+}
