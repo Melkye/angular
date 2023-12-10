a5fde16fea749b9e495d0b855ef37afc126d06e2
Denys Adamov <melkye777@gmail.com>
2023-12-11 00:14:18 +0200

    add start of lab 5 task 2

---
+++ lab5/directives2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <div>
+    <p bold>Вивчаю директиви</p>
+    <p>Створення атрибутивних директив</p>
+  </div>`,
+})
+export class AppComponent {
+  title = 'directives2';
+}
