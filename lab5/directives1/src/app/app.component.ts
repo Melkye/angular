75a57b89c3d4b855011555c2defaa4f8ffaa3715
Denys Adamov <melkye777@gmail.com>
2023-12-11 00:13:57 +0200

    finish lab5 task1

--- lab5/directives1/src/app/app.component.ts
+++ lab5/directives1/src/app/app.component.ts
@@ -3,20 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: ` <div [ngClass]="{ verdanaFont: isVerdana }">
-    <h1>Hello Angular</h1>
-    <p [ngClass]="{ segoePrintFont: isSegoe }">
-      Angular представляє модульну архітектуру додатку
-    </p>
-  </div>`,
+      <h1>Hello Angular</h1>
+      <p [ngClass]="{ segoePrintFont: isSegoe }">
+        Angular представляє модульну архітектуру додатку
+      </p>
+    </div>
+    <div [ngClass]="currentClasses">
+      <h1>Hello Angular</h1>
+      <p>Angular представляє модульну архітектуру додатку</p>
+    </div>
+    <div
+      [ngStyle]="{
+        'font-size': '13px',
+        'font-family': 'Verdana',
+        color: 'green'
+      }">
+      <h1>Вивчаємо директиви</h1>
+      <p [ngStyle]="{ 'font-size': '16px', 'font-family': 'Segoe Print' }">
+        Використання атрибутивної директиви [ngStyle]
+      </p>
+    </div>
+    <div [ngClass]="{ invisible: visibility }">
+      <h1>Вивчаємо директиви</h1>
+      <p>Використання динамічної зміни стилів</p>
+    </div>
+    <button (click)="toggle()">Toggle</button>`,
   styles: [
     `
       .verdanaFont {
-        font-size: 13px;
+        font-size: 15px;
         font-family: Verdana;
       }
       .segoePrintFont {
-        font-size: 14px;
+        font-size: 16px;
         font-family: 'Segoe Print';
+      }
+      .navyColor {
+        color: navy;
+      }
+      .invisible {
+        display: none;
       }
     `,
   ],
@@ -24,4 +50,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isVerdana = true;
   isSegoe = true;
+  isNavy = true;
+  currentClasses = {
+    verdanaFont: this.isVerdana,
+    navyColor: this.isNavy,
+  };
+  visibility: boolean = true;
+  toggle() {
+    this.visibility = !this.visibility;
+  }
 }
