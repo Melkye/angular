aa533466207c1debf032875245d7a4f1e539c65e
Denys Adamov <melkye777@gmail.com>
2023-12-16 16:18:14 +0200

    finish lab5 task2

--- lab5/directives2/src/app/app.component.ts
+++ lab5/directives2/src/app/app.component.ts
@@ -3,9 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: ` <div>
-    <p bold>Вивчаю директиви</p>
-    <p>Створення атрибутивних директив</p>
-  </div>`,
+      <p bold>Вивчаю директиви</p>
+      <p>Створення атрибутивних директив</p>
+    </div>
+    <div>
+      <p italic>Вивчаю директиви</p>
+      <p>Створення атрибутивних директив</p>
+    </div>
+    <div>
+      <p mousebold>Робота з декоратором &#64;HostListener</p>
+      <p>Реагування на курсор миші</p>
+    </div>
+    <div>
+      <p mouseitalic>Робота з декоратором &#64;HostListener</p>
+      <p>Реагування на покажчик миші при допомозі &#64;HostBinding</p>
+    </div>
+    <div>
+      <p hostmousebold>Робота з декоратором &#64;HostListener</p>
+      <p>Реагування на покажчик миші при допомозі &#64;HostBinding</p>
+    </div>`,
 })
 export class AppComponent {
   title = 'directives2';
--- lab5/directives2/src/app/app.module.ts
+++ lab5/directives2/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BoldDirective } from './bold.directive';
+import { ItalicDirective } from './italic.directive';
+import { MouseboldDirective } from './mousebold.directive';
+import { MouseitalicDirective } from './mouseitalic.directive';
+import { HostmouseboldDirective } from './hostmousebold.directive';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, BoldDirective],
+  declarations: [
+    AppComponent,
+    BoldDirective,
+    ItalicDirective,
+    MouseboldDirective,
+    MouseitalicDirective,
+    HostmouseboldDirective,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
